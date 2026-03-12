d00b71fd6b24144e8f02a67252c46291758f79ce
Miadil <kerkeb@gmail.com>
2026-03-12 09:23:24 +0100

    Tout ce qui touche aux conditions.

---
+++ main.js
@@ -0,0 +1,28 @@
+const age = 19
+
+console.log("start")
+if (age >= 18 && age <= 79 ) {
+    console.log('tu es majeur')
+} else if (age >= 80) {
+    console.log('Tu es seigneur.')
+} else {
+    console.log('tu es mineur.')
+}
+
+console.log('On est entre les if.')
+
+if (age >= 80) {
+    console.log('Tu es seigneur. 1')
+} else if (age >= 18) {
+    console.log('tu es majeur 1')
+} else {
+    console.log('tu es mieur. 1')
+}
+
+console.log('end')
+
+// const poulet = 42
+// poulet = 12
+
+// console.log(poulet)
+
