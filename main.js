1fbaffd9be9af1e1e2de9fcdfbdf408e62512f9f
Miadil <kerkeb@gmail.com>
2026-03-13 10:26:46 +0100

    on vois les boucle

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
-const age = 19
+console.log(" poulet start")
 
-console.log("start")
-if (age >= 18 && age <= 79 ) {
-    console.log('tu es majeur')
-} else if (age >= 80) {
-    console.log('Tu es seigneur.')
-} else {
-    console.log('tu es mineur.')
+// let i = 0
+
+// while (i <= 10) {
+//     console.log(i)
+    // i = i + 1
+    // i += 1
+//     i++
+// }
+
+//    i           c      i
+for(let i = 0; i <= 10; i++) {
+    console.log(i)
 }
+console.log("poulet endd")
 
-console.log('On est entre les if.')
 
-if (age >= 80) {
-    console.log('Tu es seigneur. 1')
-} else if (age >= 18) {
-    console.log('tu es majeur 1')
-} else {
-    console.log('tu es mieur. 1')
-}
+// const age = 19
 
-console.log('end')
+// console.log("start")
+// if (age >= 18 && age <= 79 ) {
+//     console.log('tu es majeur')
+// } else if (age >= 80) {
+//     console.log('Tu es seigneur.')
+// } else {
+//     console.log('tu es mineur.')
+// }
+
+// console.log('On est entre les if.')
+
+// if (age >= 80) {
+//     console.log('Tu es seigneur. 1')
+// } else if (age >= 18) {
+//     console.log('tu es majeur 1')
+// } else {
+//     console.log('tu es mieur. 1')
+// }
+
+// console.log('end')
 
 // const poulet = 42
 // poulet = 12
