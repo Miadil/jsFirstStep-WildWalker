834e610a4197d6f163fdd9f082acb698ad09103e
Miadil <kerkeb@gmail.com>
2026-03-16 10:13:07 +0100

    On vient de voir les fonctions.

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
-console.log(" poulet start")
+console.log('start');
+
+// const somme = (num1, num2 ) => {
+//     // console.log("Je suis dans la fonction SOMME.")
+//     // console.log(num1 , num2)
+//     // console.log(num1 + num2);
+//     const sommeResult = num1 + num2
+//     return sommeResult
+// }
+
+
+// function somme(num1, num2 ) {
+//     // console.log("Je suis dans la fonction SOMME.")
+//     // console.log(num1 , num2)
+//     // console.log(num1 + num2);
+//     const sommeResult = num1 + num2 // 42
+//     return sommeResult
+// }
+
+// const result1 = somme(10, 6) // 15
+// const result2 = somme(21, 21) // 42
+// // somme(10, 5) + somme(21, 21)
+// console.log("result1 : ", result1)
+// console.log("result2 : ", result2)
+
+// const resultFinal = somme(result1, result2) //57 
+
+// console.log(resultFinal);
+
+// console.log("end")
+
+// console.log(" poulet start")
 
 // let i = 0
 
@@ -10,10 +41,10 @@ console.log(" poulet start")
 // }
 
 //    i           c      i
-for(let i = 0; i <= 10; i++) {
-    console.log(i)
-}
-console.log("poulet endd")
+// for(let i = 0; i <= 10; i++) {
+//     console.log(i)
+// }
+// console.log("poulet endd")
 
 
 // const age = 19
